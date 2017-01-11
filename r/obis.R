6ab2aa6d146fe24228b22bbeb961f72cff527d33
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2017-01-11 15:46:53 -0500

    Updated obis fetch script

--- r/obis.R
+++ r/obis.R
@@ -3,6 +3,7 @@ library(robis)
 library(dplyr)
 library(purrr)
 library(parallel)
+library(rgeos)
 
 #Load Spalding's marine Ecoregions of the world
 #install_github("jebyrnes/meowR")
@@ -10,11 +11,11 @@ library(meowR)
 data(regions)
 
 #### Subset down to the ecoregions of the Americas
-americas <- c(53:58, 14, 13, 12, 11, 9, 10, 7, 8, 11, 5, 37:43, 59:61, 62:77, 152, 163:189)
+americas <- c(53:58, 5:14, 37:43, 59:61, 62:77, 152, 163:188)
 
 americas_regions <- subset(regions, regions$ECO_CODE_X %in% americas)
 
-plot(americas_regions)
+#plot(americas_regions)
 
 
 ###### Get checklist for each area
@@ -24,7 +25,7 @@ plot(americas_regions)
 #multicore, 'cause we have 20 of these to process, and each takes time
 checklists_by_region <- mclapply(1:length(americas_regions@data$ECOREGION), 
               function(x) checklist(geometry = writeWKT(americas_regions[x,])),
-              mc.cores=3)
+              mc.cores=10)
 
 names(checklists_by_region) <- as.character(americas_regions@data$ECOREGION)
 
